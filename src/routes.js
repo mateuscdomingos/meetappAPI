74aabc97998c2baa7a6195fa5e7dfa63a62e8564
Mateus Castro <mateus.castro.domingos@gmail.com>
2020-01-08 11:31:07 -0300

    Upload de arquivos finalizado

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import FileController from "./app/controllers/FileController";
 
 import authMiddlewere from "./app/middlewares/auth";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post("/sessions", SessionController.store);
-routes.use(authMiddlewere);
-
 routes.post("/users", UserController.store);
 
+routes.use(authMiddlewere);
+
 routes.put("/users", UserController.update);
+
+routes.post("/files", upload.single("file"), FileController.store);
 
 export default routes;
